c5ee9db1e38b47fe6667c2eeb2b64923d6349517
Spuds185 <Spuds185@gmail.com>
2022-01-04 11:04:40 -0600

    0.01

--- app/src/main/java/com/example/choreslist/MainActivity.java
+++ app/src/main/java/com/example/choreslist/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.choreslist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView l;
+    String MyList[] = {"This","is","a","test"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        l = findViewById(R.id.listview);
+        ArrayAdapter<String> array;
+        array = new ArrayAdapter<String>(
+          this,
+          R.layout.support_simple_spinner_dropdown_item,MyList);
+        l.setAdapter(array);
     }
 }
