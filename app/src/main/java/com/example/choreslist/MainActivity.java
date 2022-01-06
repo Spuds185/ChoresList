e83e7250a1b2b6d467189c00d0a4471533e7836d
Spuds185 <Spuds185@gmail.com>
2022-01-06 12:50:09 -0600

    0.3 Made some real progress with this one

--- app/src/main/java/com/example/choreslist/MainActivity.java
+++ app/src/main/java/com/example/choreslist/MainActivity.java
@@ -2,25 +2,44 @@ package com.example.choreslist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    //setting up a list view and mylist of stings to be used later
     ListView l;
-    String MyList[] = {"This","is","a","test"};
+    List<String> MyList = new ArrayList<String>(Collections.singleton("first"));
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //finding the list view by it's ID
         l = findViewById(R.id.listview);
+        //creating a array adapter (it seems to be more for display perposes)
         ArrayAdapter<String> array;
         array = new ArrayAdapter<String>(
           this,
           R.layout.support_simple_spinner_dropdown_item,MyList);
         l.setAdapter(array);
+
+        //creating a onclick for the addchores button
+        Button addChores = (Button) findViewById(R.id.AddChoresButton);
+        addChores.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //adding the string this to my list
+                MyList.add("this");
+                //letting the array adapter know that something changed and needs refreshing
+                array.notifyDataSetChanged();
+            }
+        });
     }
 }
